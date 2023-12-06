828b699e1c0c55a3528d05493a1b66487c6a93f3
MatiasRGT <83979853+MatiasRGT@users.noreply.github.com>
2023-12-05 23:41:42 -0300

    Informacion para hacer prestamo eventual en vivo
    usar ruts especificados en este documento.

---
+++ prestamos_en_vivo.sql
@@ -0,0 +1,15 @@
+DATOS ESTUDIANTE
+
+    rut     | nombredepila | apellido1 | apellido2 |   telefono   |              carrera               | certificadoar |           email
+------------+--------------+-----------+-----------+--------------+------------------------------------+---------------+----------------------------
+ 81041169-4 | Susana       | Águila    | Salazar   | +56969171176 | Licenciatura en Música con Mención | No            | saguila.salazar@userena.cl
+
+
+
+DATOS ENCARGADO:
+
+    rut     | nombredepila | apellido1 | apellido2
+------------+--------------+-----------+-----------
+ 16528463-4 | Sady         | Drien     | Roman
+
+
